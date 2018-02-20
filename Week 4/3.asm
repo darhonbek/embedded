ed8ee576b4e2cdcf1153570da95c7983cda42c12
Darkhonbek <darhonbek@gmail.com>
2018-02-20 21:15:55 +0500

    Update solution to new task

--- Week 4/3.asm
+++ Week 4/3.asm
@@ -2,6 +2,24 @@
 IN R20, @1
 OUT @0, R20
 .ENDMACRO
+
+;Part A
 LDI R16, 0XFF
-OUT PORTA, R16
-LOADIO PORTB, PORTA
+OUT PORTC, R16
+LOADIO PORTD, PORTC
+
+;Part B
+LDI R16, 0XFF
+OUT PORTB, R16
+LOADIO PORTE, PORTB
+
+;Part C
+LOADIO PORTD, SREG
+
+;Part D
+
+;Macro size = 4 bytes
+;Macro called 3 times => total_macro_size = 3*4 = 12 bytes
+;Additional opetations = 8 bytes
+;Total program size = 8 + 12 = 20 bytes
+;Answer: 20 bytes
--- Week 4/7.asm
+++ Week 4/7.asm
@@ -1,3 +1,10 @@
 LDI R16, HIGH(RAMEND)
 LDI R17, LOW(RAMEND)
 ; Highest accessible memory location will be stored in R16, R17 which is 0x10FF
+; Total size of data memory space:
+; (0x10FF - 0x0000) + 1byte = 4352 bytes
+
+; Size of SRAM
+; SRAM = Data Memory - 32 GPRegs + 64 I/O Regs + 160 Extended I/O Regs
+; SRAM = (4352 - 32 - 64 - 160) bytes
+; Ans: SRAM = 4096 bytes

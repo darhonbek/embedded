9a1b98381350f72f4475e520a717130ce4b64a5a
Darkhonbek <darhonbek@gmail.com>
2018-03-01 19:03:33 +0500

    Add Part 1 - D

---
+++ Week 5/part1-d.asm
@@ -0,0 +1,79 @@
+; Part 1 - D
+LDI R16, HIGH(RAMEND)
+OUT SPH, R16
+LDI R16, LOW(RAMEND)
+OUT SPL, R16
+
+LDI R17, 0xFF
+OUT DDRB, R17
+
+NEXT:
+LDI R16, 255
+OUT PORTB, R16
+RCALL DELAY
+
+LDI R16, 126
+OUT PORTB, R16
+RCALL DELAY
+
+LDI R16, 60
+OUT PORTB, R16
+RCALL DELAY
+
+LDI R16, 24
+OUT PORTB, R16
+RCALL DELAY
+
+LDI R16, 0
+OUT PORTB, R16
+RCALL DELAY
+
+LDI R16, 24
+OUT PORTB, R16
+RCALL DELAY
+
+LDI R16, 60
+OUT PORTB, R16
+RCALL DELAY
+
+LDI R16, 126
+OUT PORTB, R16
+RCALL DELAY
+
+LDI R16, 255
+OUT PORTB, R16
+RCALL DELAY
+
+RJMP NEXT
+
+END: 
+RJMP END
+
+DELAY:                 
+LDI R20, 76       
+D1:
+LDI R21, 101     
+D2:
+LDI R22, 113     
+D3:
+DEC R22            
+NOP            
+NOP            
+NOP            
+NOP            
+NOP            
+NOP            
+NOP            
+NOP            
+NOP            
+NOP            
+NOP            
+NOP            
+NOP                     
+BRNE D3          
+DEC R21            
+BRNE D2          
+DEC R20            
+BRNE D1          
+        
+RET
